b0afec240c23af010338d34ceefdd3a5d7e2190c
GeorgeH23 <gheorghita.harpa0423@yahoo.com>
2024-02-20 22:09:06 +0000

    fix: Working on deploy #9

--- depo_pc/settings.py
+++ depo_pc/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['depo-pc-1fa940aecc2f.herokuapp.com/', '8000-georgeh23-depo-pc-3jx0j27gj6.us2.codeanyapp.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['depo-pc.herokuapp.com', '8000-georgeh23-depo-pc-3jx0j27gj6.us2.codeanyapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -103,16 +103,17 @@ WSGI_APPLICATION = 'depo_pc.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
