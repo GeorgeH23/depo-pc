63ef14998a4ed57f2ba6b656b7ecda2deeab6954
GeorgeH23 <gheorghita.harpa0423@yahoo.com>
2024-02-20 21:45:50 +0000

    fix: Correct the heroku link #9

--- depo_pc/settings.py
+++ depo_pc/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://depo-pc-1fa940aecc2f.herokuapp.com/', '8000-georgeh23-depo-pc-3jx0j27gj6.us2.codeanyapp.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['depo-pc-1fa940aecc2f.herokuapp.com/', '8000-georgeh23-depo-pc-3jx0j27gj6.us2.codeanyapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
