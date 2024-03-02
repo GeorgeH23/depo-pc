72a7836dc9072f2ff66692ce20ce995f08be3ca6
GeorgeH23 <gheorghita.harpa0423@yahoo.com>
2024-03-02 01:41:56 +0000

    feat: Add check for out of stock #23

--- products/models.py
+++ products/models.py
@@ -22,6 +22,7 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    in_stock = models.BooleanField(default=False, null=True, blank=True)
     image = models.URLField(max_length=1024, null=True, blank=True)
     stars = models.DecimalField(max_digits=6, decimal_places=2, null=True,
                                 blank=True)
