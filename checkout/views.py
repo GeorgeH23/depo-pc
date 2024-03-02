c740ecb91a3cead0ba6266e1d2579b75d95974eb
GeorgeH23 <gheorghita.harpa0423@yahoo.com>
2024-03-02 17:06:46 +0000

    feat: Add Stripe elements #24

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51OpuqeHySjuRs6tM9u8CNH4qyP6OTwoEowP7NKeDeGIn5raepweDE5gY3wlvmJaIjQaV4Pk3cJDMTKQpJr0gek5r00dH3xaG8a',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
