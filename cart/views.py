0a52502ab2bd0ce9bbd59ebf104577784a0ea869
GeorgeH23 <gheorghita.harpa0423@yahoo.com>
2024-03-02 02:27:25 +0000

    feat: Finish the in stock logic #23

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from products.models import Product
+from django.contrib import messages
 
 # Create your views here.
 
@@ -8,16 +10,39 @@ def view_cart(request):
 
 
 def add_to_cart(request, item_id):
-    """ Add a quantity of the specified product to the shopping cart """
-
-    quantity = int(request.POST.get('quantity'))
+    quantity_str = request.POST.get('quantity')
     redirect_url = request.POST.get('redirect_url')
+    stock = None
+    
+    if 'in_stock' in request.POST:
+        stock = request.POST['in_stock']
+
+    if not quantity_str or not quantity_str.isdigit():
+        messages.error(request, 'Invalid quantity.')
+        return redirect(redirect_url)
+
+    quantity = int(quantity_str)
+
     cart = request.session.get('cart', {})
 
-    if item_id in list(cart.keys()):
-        cart[item_id] += quantity
+    if stock:
+        if item_id in list(cart.keys()):
+            if stock in cart[item_id]['items_stock'].keys():
+                cart[item_id]['items_stock'][stock] += quantity
+            else:
+                cart[item_id]['items_stock'][stock] = quantity
+        else:
+            cart[item_id] = {'items_stock': {stock: quantity}}
     else:
-        cart[item_id] = quantity
+        if item_id in list(cart.keys()):
+            cart[item_id] += quantity
+        else:
+            cart[item_id] = quantity
 
     request.session['cart'] = cart
+
+    product = get_object_or_404(Product, pk=item_id)
+    if not product.in_stock:
+        messages.warning(request, 'This product is out of stock.')
+
     return redirect(redirect_url)
