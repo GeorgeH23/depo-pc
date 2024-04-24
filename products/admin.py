a443b281df1b6b6a491f60bbd728814b68b2bf3b
GeorgeH23 <gheorghita.harpa0423@yahoo.com>
2024-04-24 17:36:26 +0100

    feat: Add Review to admin site #35

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category, Favorite
+from .models import Product, Category, Favorite, Review
 
 # Register your models here.
 
@@ -36,3 +36,4 @@ class FavoriteAdmin(admin.ModelAdmin):
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Favorite, FavoriteAdmin)
+admin.site.register(Review)
