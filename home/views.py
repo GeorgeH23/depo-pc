d9c684e8ce69d5f750169be57ead5fe31da90875
GeorgeH23 <gheorghita.harpa0423@yahoo.com>
2024-04-28 22:07:06 +0100

    feat: Add carousel slides on home page #42

--- home/views.py
+++ home/views.py
@@ -4,4 +4,19 @@ from django.shortcuts import render
 
 def index(request):
     """ A view to return the index page """
-    return render(request, 'home/index.html')
+    num_items = 3 
+    captions = [
+        {'title': 'Desktop PCs'},
+        {'title': 'Laptops'},
+        {'title': 'Accessories'}
+    ]
+
+    for i in range(len(captions)):
+        captions[i]['index'] = i + 1
+
+    context = {
+        'num_items': num_items,
+        'captions': captions,
+    }
+
+    return render(request, 'home/index.html', context)
